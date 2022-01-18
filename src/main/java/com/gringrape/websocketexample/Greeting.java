147609701a302ec7e756dd44c384d152c3e25a35
gringrape <gringrape200@gmail.com>
2022-01-19 08:58:01 +0900

    Ing...

---
+++ src/main/java/com/gringrape/websocketexample/Greeting.java
@@ -0,0 +1,16 @@
+package com.gringrape.websocketexample;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ src/main/java/com/gringrape/websocketexample/HelloController.java
@@ -0,0 +1,17 @@
+package com.gringrape.websocketexample;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class HelloController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws InterruptedException {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!!!");
+    }
+}
